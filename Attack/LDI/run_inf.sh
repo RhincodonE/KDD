e7bfd3039593087a8bad82f85c0b8307ad6e7b24
RhincodonE <111275412+RhincodonE@users.noreply.github.com>
2025-01-28 08:50:20 -0600

    Delete Attack directory

--- Attack/LDI/run_inf.sh
+++
@@ -1,8 +0,0 @@
-yaml_file="./configs/config_inf.yaml"
-
-
-#python ./scripts/generate_inf_diction.py --config-file $yaml_file
-python ./scripts/generate_inf.py --config-file $yaml_file
-
-
-echo "Inf file dictionary generating finished!"
